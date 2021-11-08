52c5ffc70094afabae92dfbdaa395b9265bbe5c2
nachelysanchez <nashely-5@hotmail.com>
2021-11-08 10:54:23 -0400

    Creando la clase TiposTareasBLL

---
+++ BLL/TiposTareasBLL.cs
@@ -0,0 +1,71 @@
+﻿using P2_AP1_Nachely_20190734.DAL;
+using P2_AP1_Nachely_20190734.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_AP1_Nachely_20190734.BLL
+{
+    public class TiposTareasBLL
+    {
+        public static TiposTareas Buscar(int id)
+        {
+            TiposTareas tarea;
+            Contexto contexto = new Contexto();
+            try
+            {
+                tarea = contexto.TiposTareas.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return tarea;
+        }
+
+        public static List<TiposTareas> GetTiposTareas()
+        {
+            Contexto contexto = new Contexto();
+            List<TiposTareas> lista = new List<TiposTareas>();
+            try
+            {
+                lista = contexto.TiposTareas.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+
+        public static List<TiposTareas> GetList(Expression<Func<TiposTareas, bool>> criterio)
+        {
+            Contexto contexto = new Contexto();
+            List<TiposTareas> lista = new List<TiposTareas>();
+            try
+            {
+                lista = contexto.TiposTareas.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+    }
+}
