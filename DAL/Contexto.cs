0345c302b57ece4b18cde367972e44a2fade1367
nachelysanchez <nashely-5@hotmail.com>
2021-11-08 10:36:48 -0400

    Creando el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P2_AP1_Nachely_20190734.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_AP1_Nachely_20190734.DAL
+{
+    public class Contexto : DbContext
+    {
+        public  DbSet<TiposTareas> TiposTareas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DATA\ProyectosControl.db");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TiposTareas>().HasData(new TiposTareas
+            {
+                TipoTareaId = 1,
+                DescripcionTipoTarea = "Análisis",
+                TiempoAcumulado = 0
+            });
+            modelBuilder.Entity<TiposTareas>().HasData(new TiposTareas
+            {
+                TipoTareaId = 2,
+                DescripcionTipoTarea = "Diseño",
+                TiempoAcumulado = 0
+            });
+            modelBuilder.Entity<TiposTareas>().HasData(new TiposTareas
+            {
+                TipoTareaId = 3,
+                DescripcionTipoTarea = "Programación",
+                TiempoAcumulado = 0
+            });
+            modelBuilder.Entity<TiposTareas>().HasData(new TiposTareas
+            {
+                TipoTareaId = 4,
+                DescripcionTipoTarea = "Prueba",
+                TiempoAcumulado = 0
+            });
+        }
+    }
+}
