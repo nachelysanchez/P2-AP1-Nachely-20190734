f3289d93b254e5a2431fd3b43fb6dc9a523f69cb
nachelysanchez <nashely-5@hotmail.com>
2021-11-08 11:56:55 -0400

    Cargando el ComboBox de TipoTareas con sus opciones correspondientes.

--- UI/Registros/rProyectos.xaml.cs
+++ UI/Registros/rProyectos.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using P2_AP1_Nachely_20190734.BLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,40 @@ namespace P2_AP1_Nachely_20190734.UI.Registros
         public rProyectos()
         {
             InitializeComponent();
+
+            TipoTareaComboBox.ItemsSource = TiposTareasBLL.GetTiposTareas();
+            TipoTareaComboBox.SelectedValuePath = "TipoTareaId";
+            TipoTareaComboBox.DisplayMemberPath = "DescripcionTipoTarea";
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AgregarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoverFilaButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
